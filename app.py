2d076a1d4a812bb27e7d63427beee12a8434e149
Naresh <82667769+knaresh10@users.noreply.github.com>
2023-06-23 14:06:47 +0530

    update files location

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
+import torch
+from transformers import BartForConditionalGeneration, BartTokenizer
 from flask import Flask, render_template, request
-from textSummarizer import summarizer
+model = BartForConditionalGeneration.from_pretrained(r'D:\projects\Abstractive Summarization Project Data\model')
+tokenizer = BartTokenizer.from_pretrained(r'D:\projects\Abstractive Summarization Project Data\model')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
 
 app = Flask(__name__)
 
-
 @app.route('/')
+
+
 @app.route('/res', methods=['GET', 'POST'])
 def index():
-
-    textarea_data = ""
-    output_data = ""
+    
+    textarea_data =""
+    output_data=""
     if request.method == 'POST':
-
+    
         textarea_data = request.form['input']
-        output_data = summarizer(textarea_data)
+        input_ids = tokenizer.encode(textarea_data, return_tensors='pt').to(device)
+        summary_ids = model.generate(input_ids, num_beams=4, max_length=100, early_stopping=True)
+        output_data = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
     return render_template("index.html", input_data=textarea_data, output_data=output_data)
